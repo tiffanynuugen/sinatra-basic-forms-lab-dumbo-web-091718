b99f85da61b95dbb7755f9bbdd245fbb0db4e704
tiffanynuugen <tiffanynuugen@gmail.com>
2018-10-10 16:14:40 -0400

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,16 @@
+class Puppy
+
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  # def self.create(params)
+  #   @puppy = Puppy.new(params)
+  #   @puppy.save
+  # end
+
+end
